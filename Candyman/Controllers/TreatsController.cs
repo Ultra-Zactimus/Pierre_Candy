32c47a72f2fe8dc4a4ff62f1117d1377317c81c6
Ultra Zacky <ultrazacky@gmail.com>
2022-03-25 11:40:02 -0700

    add DeleteFlavor control to Treat Views

--- Candyman/Controllers/TreatsController.cs
+++ Candyman/Controllers/TreatsController.cs
@@ -113,5 +113,14 @@ namespace Candyman.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.TreatFlavor.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavor.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
