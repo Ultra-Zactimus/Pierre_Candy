5dfd89ce2df2b8266e815416706fa4c5cb237dea
Ultra Zacky <ultrazacky@gmail.com>
2022-03-25 11:03:02 -0700

    add Details control for Treats Views

--- Candyman/Controllers/TreatsController.cs
+++ Candyman/Controllers/TreatsController.cs
@@ -53,6 +53,15 @@ namespace Candyman.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntities)
+          .ThenInclude(join => join.Flavor)
+          .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
     
   }
 }
