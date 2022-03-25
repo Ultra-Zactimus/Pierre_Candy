6a867e1eb68dfae00a4d6399290a1caf9d505b3c
Ultra Zacky <ultrazacky@gmail.com>
2022-03-25 10:48:11 -0700

    add Index control for Treats Views

--- Candyman/Controllers/TreatsController.cs
+++ Candyman/Controllers/TreatsController.cs
@@ -9,3 +9,26 @@ using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
 
+namespace Candyman.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly CandymanContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, CandymanContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userTreats);
+    }
+  }
+}
