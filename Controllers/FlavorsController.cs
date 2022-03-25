408d4aa3cb8f47b0569aa47fc2987a8e5c7303e8
Ultra Zacky <ultrazacky@gmail.com>
2022-03-25 09:58:01 -0700

    add Create control for Flavor Views

--- Controllers/FlavorsController.cs
+++ Controllers/FlavorsController.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Library.Models;
+using Candyman.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,6 +18,19 @@ namespace Candyman.Controllers
     {
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
     
