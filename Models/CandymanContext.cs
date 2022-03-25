616b03855db5c2ae6210121b958ab0015bd204a9
Ultra Zacky <ultrazacky@gmail.com>
2022-03-25 09:17:43 -0700

    add Context code for creating tables in mySQL

---
+++ Models/CandymanContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Candyman.Models
+{
+  public class CandymanContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public CandymanContext(DbContextOptions options): base(options) { }
+    public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
